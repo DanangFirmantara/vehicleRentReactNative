c1bc6eb420a545455d07b28f1a0d3e9612c399d2
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-01 21:44:28 +0700

    feat : slicing add item

---
+++ src/components/BackItem.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const BackItem = () => {
+  return (
+    <View>
+      <Text>BackItem</Text>
+    </View>
+  );
+};
+
+export default BackItem;
--- src/components/ButtonForm.js
+++ src/components/ButtonForm.js
@@ -1,16 +1,26 @@
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/AntDesign';
+import {useNavigation} from '@react-navigation/native';
 
 const ButtonForm = ({
   children,
   color = 'rgba(255, 205, 97, 1)',
   textColor = 'rgba(57, 57, 57, 1)',
   google = null,
+  to = null,
 }) => {
+  const navigation = useNavigation();
+  const onPress = () => {
+    if (to) {
+      navigation.navigate(to);
+    }
+  };
   return (
     <View style={buttonStyle.parent}>
-      <TouchableOpacity style={[buttonStyle.item, {backgroundColor: color}]}>
+      <TouchableOpacity
+        style={[buttonStyle.item, {backgroundColor: color}]}
+        onPress={onPress}>
         {google && <Icon name="google" size={30} />}
         <Text style={[buttonStyle.textItem, {color: textColor}]}>
           {children}
@@ -33,7 +43,6 @@ const buttonStyle = StyleSheet.create({
   textItem: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginHorizontal: 10,
   },
 });
 
