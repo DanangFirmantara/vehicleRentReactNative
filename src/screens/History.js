c7cb3b2ee2628fad554c580aa366bcee5ab3ec6e
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-02 15:16:18 +0700

    feat : add history

---
+++ src/screens/History.js
@@ -0,0 +1,157 @@
+import {
+   Text,
+   FlatList,
+   Pressable,
+   Box,
+   HStack,
+   VStack,
+   Image,
+} from 'native-base';
+import Icon from 'react-native-vector-icons/Entypo';
+import React from 'react';
+import Container from '../components/Container';
+import imgVehicle from '../assets/image/profile.png';
+
+const History = () => {
+   const data = [
+      {
+         menu: 'User 3 has finished the payment',
+      },
+      {
+         menu: 'User 3’s payment for a vintage bike at Jogja just confirmed!',
+      },
+   ];
+   const history = [
+      {
+         img: imgVehicle,
+         name: 'Vespa Matic',
+         prepayment: 245000,
+         status: 'Finished',
+         id: 1,
+      },
+      {
+         img: imgVehicle,
+         name: 'Vespa Matic',
+         prepayment: 245000,
+         status: 'Finished',
+         id: 2,
+      },
+      {
+         img: imgVehicle,
+         name: 'Vespa Matic',
+         prepayment: 245000,
+         status: 'Finished',
+         id: 3,
+      },
+      {
+         img: imgVehicle,
+         name: 'Vespa Matic',
+         prepayment: 245000,
+         status: 'Finished',
+         id: 4,
+      },
+      {
+         img: imgVehicle,
+         name: 'Vespa Matic',
+         prepayment: 245000,
+         status: 'Finished',
+         id: 5,
+      },
+      {
+         img: imgVehicle,
+         name: 'Vespa Matic',
+         prepayment: 245000,
+         status: 'Finished',
+         id: 6,
+      },
+      {
+         img: imgVehicle,
+         name: 'Vespa Matic',
+         prepayment: 245000,
+         status: 'Finished',
+         id: 7,
+      },
+   ];
+   return (
+      <Container>
+         <Box>
+            <Text fontSize={18} color={'rgba(196, 196, 196, 1)'} marginTop={5}>
+               Today
+            </Text>
+            <FlatList
+               data={data}
+               renderItem={({item}) => {
+                  return (
+                     <Pressable>
+                        {({isHovered, isFocused, isPressed}) => {
+                           return (
+                              <Box
+                                 py="2"
+                                 bg={
+                                    isPressed
+                                       ? 'coolGray.200'
+                                       : isHovered
+                                       ? 'coolGray.200'
+                                       : 'transparent'
+                                 }>
+                                 <HStack alignItems={'center'}>
+                                    <Text
+                                       fontSize={18}
+                                       fontWeight={600}
+                                       flex={1}
+                                       color={'rgba(57, 57, 57, 1)'}>
+                                       {item.menu}
+                                    </Text>
+                                    <Icon name="chevron-right" size={30} />
+                                 </HStack>
+                              </Box>
+                           );
+                        }}
+                     </Pressable>
+                  );
+               }}
+            />
+            <Text fontSize={18} color={'rgba(196, 196, 196, 1)'} my={5}>
+               A week ago
+            </Text>
+            <FlatList
+               data={history}
+               showsVerticalScrollIndicator={false}
+               renderItem={({item}) => {
+                  return (
+                     <Box my={4}>
+                        <HStack alignItems={'center'} key={item.id}>
+                           <Box mr={5}>
+                              <Image
+                                 source={item.img}
+                                 width={100}
+                                 height={88}
+                                 borderRadius={15}
+                                 alt={String(item.id)}
+                              />
+                           </Box>
+                           <VStack space={1}>
+                              <Text
+                                 color={'rgba(57, 57, 57, 1)'}
+                                 fontWeight={'bold'}>
+                                 {item.name}
+                              </Text>
+                              <Text>Jan 18 to 21 2021</Text>
+                              <Text
+                                 color={'rgba(57, 57, 57, 1)'}
+                                 fontWeight={'bold'}>
+                                 Prepayment : Rp.{item.prepayment}
+                              </Text>
+                              <Text color={'green.900'}>{item.status}</Text>
+                           </VStack>
+                        </HStack>
+                     </Box>
+                  );
+               }}
+            />
+         </Box>
+      </Container>
+   );
+};
+
+export default History;
--- src/screens/HomeAdmin.js
+++ src/screens/HomeAdmin.js
@@ -5,60 +5,65 @@ import imgHeader from '../assets/image/home.png';
 import ButtonForm from '../components/ButtonForm';
 
 const HomeAdmin = () => {
-  const data = [
-    {
-      alt: 'img1',
-      img: imgHeader,
-    },
-    {
-      alt: 'img2',
-      img: imgHeader,
-    },
-    {
-      alt: 'img3',
-      img: imgHeader,
-    },
-  ];
-  return (
-    <>
-      <Image source={imgHeader} width={'100%'} alt="imgHeader" height={267} />
-      <Container>
-        <HStack space={5} marginTop={3}>
-          <Input placeholder={'Select Location'} flex={1} />
-          <Input placeholder={'Car'} width={100} />
-        </HStack>
-        <ButtonForm>Search Vehicle</ButtonForm>
-        <HStack>
-          <Text flex={1} fontWeight="bold">
-            Recommended
-          </Text>
-          <Text underline={'true'} fontWeight="bold">
-            View More
-          </Text>
-        </HStack>
-        <Box marginY={2} flex={1}>
-          <FlatList
-            horizontal={true}
-            data={data}
-            renderItem={({item}) => (
-              <Box>
-                <HStack>
-                  <Image source={item.img} alt={item.alt} />
-                </HStack>
-              </Box>
-            )}
-          />
-        </Box>
+   const data = [
+      {
+         alt: 'img1',
+         img: imgHeader,
+      },
+      {
+         alt: 'img2',
+         img: imgHeader,
+      },
+      {
+         alt: 'img3',
+         img: imgHeader,
+      },
+   ];
+   return (
+      <>
+         <Image
+            source={imgHeader}
+            width={'100%'}
+            alt="imgHeader"
+            height={267}
+         />
+         <Container>
+            <HStack space={5} marginTop={3}>
+               <Input placeholder={'Select Location'} flex={1} />
+               <Input placeholder={'Car'} width={100} />
+            </HStack>
+            <ButtonForm>Search Vehicle</ButtonForm>
+            <HStack>
+               <Text flex={1} fontWeight="bold">
+                  Recommended
+               </Text>
+               <Text underline={'true'} fontWeight="bold">
+                  View More
+               </Text>
+            </HStack>
+            <Box marginY={2} flex={1}>
+               <FlatList
+                  horizontal={true}
+                  data={data}
+                  renderItem={({item}) => (
+                     <Box>
+                        <HStack>
+                           <Image source={item.img} alt={item.alt} />
+                        </HStack>
+                     </Box>
+                  )}
+               />
+            </Box>
 
-        <ButtonForm
-          color="rgba(57, 57, 57, 1)"
-          textColor="rgba(255, 205, 97, 1)"
-          to={'AddNewItem'}>
-          Add new item
-        </ButtonForm>
-      </Container>
-    </>
-  );
+            <ButtonForm
+               color="rgba(57, 57, 57, 1)"
+               textColor="rgba(255, 205, 97, 1)"
+               to={'AddNewItem'}>
+               Add new item
+            </ButtonForm>
+         </Container>
+      </>
+   );
 };
 
 export default HomeAdmin;
