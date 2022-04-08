9bc8996616bb6ee90f04c9ebd75885c660cf5426
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-09 00:36:18 +0700

    feat : add notification folder

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import reduxStore from './src/redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import messaging from '@react-native-firebase/messaging';
-import PushNotification from 'react-native-push-notification';
 import Main from './src/routes';
+import './src/Notifications';
 
 const { store, persistor } = reduxStore();
-
-PushNotification.createChannel({
-   channelId: 'profile-notif',
-   channelName: 'for test notification',
-   vibrate: true,
-   playSound: true,
-   soundName: 'default',
-});
 
 const App = () => {
    const getToken = async () => {
