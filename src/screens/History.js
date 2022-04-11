7c8d33f5232cd5e6c415e49029a2d14140768f4b
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-11 16:47:58 +0700

    feat : add redux on history

--- src/screens/History.js
+++ src/screens/History.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
    Text,
    FlatList,
@@ -8,68 +9,27 @@ import {
    Image,
 } from 'native-base';
 import Icon from 'react-native-vector-icons/Entypo';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from '../components/Container';
 import imgVehicle from '../assets/image/profile.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { getHistory } from '../redux/actions/history';
+import { BACKEND_URL } from '../../env';
 
 const History = () => {
+   const dispatch = useDispatch();
+   const auth = useSelector((state) => state.auth);
+   const history = useSelector((state) => state.history);
+   useEffect(() => {
+      dispatch(getHistory(auth.token));
+   }, [dispatch]);
+
    const data = [
       {
          menu: 'User 3 has finished the payment',
       },
       {
          menu: 'User 3’s payment for a vintage bike at Jogja just confirmed!',
-      },
-   ];
-   const history = [
-      {
-         img: imgVehicle,
-         name: 'Vespa Matic',
-         prepayment: 245000,
-         status: 'Finished',
-         id: 1,
-      },
-      {
-         img: imgVehicle,
-         name: 'Vespa Matic',
-         prepayment: 245000,
-         status: 'Finished',
-         id: 2,
-      },
-      {
-         img: imgVehicle,
-         name: 'Vespa Matic',
-         prepayment: 245000,
-         status: 'Finished',
-         id: 3,
-      },
-      {
-         img: imgVehicle,
-         name: 'Vespa Matic',
-         prepayment: 245000,
-         status: 'Finished',
-         id: 4,
-      },
-      {
-         img: imgVehicle,
-         name: 'Vespa Matic',
-         prepayment: 245000,
-         status: 'Finished',
-         id: 5,
-      },
-      {
-         img: imgVehicle,
-         name: 'Vespa Matic',
-         prepayment: 245000,
-         status: 'Finished',
-         id: 6,
-      },
-      {
-         img: imgVehicle,
-         name: 'Vespa Matic',
-         prepayment: 245000,
-         status: 'Finished',
-         id: 7,
       },
    ];
    return (
@@ -116,18 +76,29 @@ const History = () => {
                A week ago
             </Text>
             <FlatList
-               data={history}
+               data={history.data.length > 0 ? history.data : null}
                showsVerticalScrollIndicator={false}
+               ListFooterComponent={<Box mb={280} />}
                renderItem={({ item }) => {
                   return (
                      <Box my={2}>
                         <HStack alignItems={'center'} key={item.id}>
                            <Box mr={5}>
                               <Image
-                                 source={item.img}
+                                 source={
+                                    item.image
+                                       ? {
+                                            uri: item.image.replace(
+                                               'http://localhost:5000',
+                                               BACKEND_URL,
+                                            ),
+                                         }
+                                       : imgVehicle
+                                 }
                                  width={100}
                                  height={88}
                                  borderRadius={15}
+                                 resizeMode={'contain'}
                                  alt={String(item.id)}
                               />
                            </Box>
@@ -135,15 +106,22 @@ const History = () => {
                               <Text
                                  color={'rgba(57, 57, 57, 1)'}
                                  fontWeight={'bold'}>
-                                 {item.name}
+                                 {item.vehicleName}
                               </Text>
-                              <Text>Jan 18 to 21 2021</Text>
+                              <Text>
+                                 {item.rentStartDate} to {item.rentEndDate}
+                              </Text>
                               <Text
                                  color={'rgba(57, 57, 57, 1)'}
                                  fontWeight={'bold'}>
-                                 Prepayment : Rp.{item.prepayment}
+                                 Prepayment : Rp.{item.total}
                               </Text>
-                              <Text color={'green.900'}>{item.status}</Text>
+                              <Text
+                                 color={
+                                    item.status === 1 ? 'green.800' : 'red.800'
+                                 }>
+                                 {item.status === 1 ? 'Paid' : 'Not Paid'}
+                              </Text>
                            </VStack>
                         </HStack>
                      </Box>
