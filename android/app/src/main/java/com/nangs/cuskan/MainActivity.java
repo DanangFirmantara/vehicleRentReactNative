2dd2bc452461ad3daf33e3d5eedd059959c5af3e
DanangFirmantara <danangfirmantara837@gmail.com>
2022-04-16 12:31:44 +0700

    feat : add splash-screen

--- android/app/src/main/java/com/nangs/cuskan/MainActivity.java
+++ android/app/src/main/java/com/nangs/cuskan/MainActivity.java
@@ -2,6 +2,7 @@ package com.nangs.cuskan;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -9,12 +10,14 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
-  @Override
-  protected String getMainComponentName() {
-    return "CusKan";
-  }
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(null);
+   }
+   
+   @Override
+   protected String getMainComponentName() {
+      return "CusKan";
+   }
 }
